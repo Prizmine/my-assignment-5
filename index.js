aa07927113248d1da046031cf215282d60b33490
Prizmine <prizmine123@gmail.com>
2025-08-28 11:39:36 +0600

    added coin function

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
+
+// copy-button
+
 let heartCount = 0;
 
-document.querySelectorAll(".heart-button").forEach(button => {
-  button.addEventListener("click", function () {
-    heartCount++
+let heartButtons = document.querySelectorAll(".heart-button");
+
+for (let i = 0; i < heartButtons.length; i++) {
+  heartButtons[i].addEventListener("click", function () {
+    heartCount++;
     document.querySelector(".heart-count-number").innerText = heartCount;
   });
-});
+}
+
+let callButtons = document.querySelectorAll(".call-button")
+
+
+for (let i = 0; i< callButtons.length; i++){
+    callButtons[i].addEventListener("click", function(){
+
+        let coin = Number(document.getElementsByClassName('default-coin')[0].innerText)
+        if(coin >= 20){
+            let card = callButtons[i].parentElement.parentElement;
+            let title = card.querySelector(".card-subtitle").innerText;
+            let number = card.querySelector(".contact-num").innerText;
+            alert(`📞Calling ${title} ${number}`);
+            document.getElementsByClassName('default-coin')[0].innerText = coin - 20;
+        }
+        else{
+          alert('Not enough coins')
+        }
+    })
+}
+
+
