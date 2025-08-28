4159951ef3e376537edb69275ffa365709e3f085
Prizmine <prizmine123@gmail.com>
2025-08-28 22:32:36 +0600

    added call function

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ for (let i = 0; i < heartButtons.length; i++) {
   });
 }
 
+
+
 let callButtons = document.querySelectorAll(".call-button")
 
 
@@ -23,8 +25,85 @@ for (let i = 0; i< callButtons.length; i++){
             let card = callButtons[i].parentElement.parentElement;
             let title = card.querySelector(".card-subtitle").innerText;
             let number = card.querySelector(".contact-num").innerText;
+            let mainTitle = card.querySelector(".card-title").innerText;
             alert(`📞Calling ${title} ${number}`);
             document.getElementsByClassName('default-coin')[0].innerText = coin - 20;
+
+
+            
+            
+            let inCallDataLeft1 = document.createElement("p");
+            inCallDataLeft1.innerText = mainTitle;
+            inCallDataLeft1.style.fontSize = "18px";
+            inCallDataLeft1.style.fontWeight = "400";
+
+            let inCallDataLeft2 = document.createElement("p");
+            inCallDataLeft2.innerText = number;
+            inCallDataLeft2.style.fontSize = "18px";
+            inCallDataLeft2.style.fontWeight ="400";
+            inCallDataLeft2.style.marginTop = "4px";
+
+
+
+            let callDataLeft = document.createElement("div");
+            callDataLeft.style.display = "flex";
+            callDataLeft.style.flexDirection = "column";
+            callDataLeft.appendChild(inCallDataLeft1);
+            callDataLeft.appendChild(inCallDataLeft2);
+
+            
+            let now = new Date();
+            let hours = now.getHours();
+            let minutes = now.getMinutes();
+            let ampm;
+
+            if (hours >= 12) {
+              ampm = "PM";
+
+              } else {
+
+                ampm = "AM";
+          }
+
+
+
+            hours = hours % 12;
+            if (hours === 0) {
+            hours = 12;
+            }
+
+            if (minutes < 10) {
+            minutes = "0" + minutes;
+            }
+
+            let callDataRight = document.createElement("div");
+            callDataRight.innerText = `${hours}:${minutes} ${ampm}`;
+
+            
+
+
+            let callDataDiv = document.createElement("div");
+            callDataDiv.style.alignItems = "center"
+            callDataDiv.appendChild(callDataLeft);
+            callDataDiv.appendChild(callDataRight);
+            callDataDiv.style.justifyContent = "space-between";
+
+
+            let callData = document.getElementById('Call-data-container').appendChild(callDataDiv);
+            callData.style.height = "86px"
+            callData.style.maxWidth = "352px";
+            callData.style.display = "flex";
+            callData.style.backgroundColor = "#F2F2F2";
+            callData.style.margin = "0 auto"
+            callData.style.padding = "16px"
+            callData.style.borderRadius = "8px"
+            callData.style.marginTop = "8px"
+            callData.appendChild(callDataDiv)
+
+
+
+
+            
         }
         else{
           alert('Not enough coins')
